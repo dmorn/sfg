45a1731cb0e85563a2d8a4377d2c964dfcb6dc2d
Daniel Morandini <danielmorandini@me.com>
2020-03-29 16:25:40 +0200

    Add some crop facts

---
+++ src/sfg.pl
@@ -0,0 +1,14 @@
+% SPDX-FileCopyrightText: 2020 jecoz
+%
+% SPDX-License-Identifier: MIT
+
+crop(cocumbers).
+crop(carrots).
+crop(green_beans).
+crop(radishes).
+crop(sweet_corn).
+crop(peppers).
+crop(lettuce).
+crop(onions).
+crop(squash).
+crop(tomatoes).

2d1586c4243eb461352b47f590f32dd6f746c199
Daniel Morandini <danielmorandini@me.com>
2020-03-29 17:02:14 +0200

    Add companion facts

--- src/sfg.pl
+++ src/sfg.pl
@@ -2,13 +2,41 @@
 %
 % SPDX-License-Identifier: MIT
 
-crop(cocumbers).
-crop(carrots).
-crop(green_beans).
-crop(radishes).
-crop(sweet_corn).
-crop(peppers).
-crop(lettuce).
-crop(onions).
-crop(squash).
-crop(tomatoes).
+crop(cocumber).   % cucurbitacee
+crop(carrot).     % apiacee
+crop(green_bean). % fabacee
+crop(radishe).    % brassicaceae
+crop(sweet_corn). % poaceae
+crop(pepper).     % solanacee
+crop(lettuce).    % asteracee
+crop(onion).      % amaryllidaceae
+crop(squash).     % cucurbitacee (zucchini)
+crop(tomatoe).    % solanacee
+
+likes(cocumber, green_bean).
+likes(cocumber, sweet_corn).
+likes(cocumber, lettuce).
+likes(carrots, tomatoes).
+likes(green_bean, sweet_corn).
+likes(green_bean, cocumber).
+likes(green_bean, radish).
+likes(radish, cocumber).
+likes(radish, carrot).
+likes(radish, lettuce).
+likes(radish, squash).
+likes(sweet_corn, green_bean).
+likes(sweet_corn, cocumber).
+likes(sweet_corn, squash).
+likes(pepper, basil).
+likes(pepper, tomato).
+likes(lettuce, carrot).
+likes(lettuce, sweet_corn).
+likes(lettuce, radish).
+likes(onion, carrot).
+likes(onion, lettuce).
+likes(onion, tomato).
+likes(squash, sweet_corn).
+likes(squash, radish).
+likes(tomato, carrot).
+likes(tomato, onion).
+likes(tomato, lettuce).
